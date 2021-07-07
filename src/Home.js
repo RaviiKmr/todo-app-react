ba71afacf35e27501304abe4286404adcbedc666
Ravi Das <iraviikmr@gmail.com>
2021-07-07 22:43:02 +0530

    new changes

--- src/Home.js
+++ src/Home.js
@@ -1,43 +1,88 @@
 import { useState, useEffect } from "react";
 
-const Home = () =>{
+const Home = () => {
 
     const clickMe = () => {
         alert('hello');
-      }
+    }
 
     const [lists, setLists] = useState(null);
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/todos/')
-        .then(res => {
-            return res.json();
-        })
-        .then(data =>{
-            setLists(data)
-        });
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setLists(data)
+            });
     }, [])
 
     return (
         <div className="home">
-            <form>
-                <input placeholder="enter task">
-                </input>
-                <button type="submit">add</button>
-            </form>
-            <div className="TaskList">
-                {lists && <ul>
-                {lists.map((list) =>(
-                <div className="list-item" key={list.id}>
-                    <li>{ list.title }<button onClick={clickMe}>X</button><button>e</button></li>
-                </div>
-                ))}
-                
-                </ul>}
+            <div className="p-4 h1 text-primary text-center mx-auto display-inline-block">
+                <i className="fa fa-check bg-primary text-white rounded p-2"></i>
+                <u> TODO List</u>
             </div>
-            
+
+            <div className="row m-1 p-3">
+                <div className="col col-11 mx-auto">
+                    <div className="row bg-white rounded shadow-sm p-2 add-todo-wrapper align-items-center justify-content-center">
+                        <div className="col">
+                            <input className="form-control form-control-lg border-0 add-todo-input bg-transparent rounded" type="text" placeholder="Add new .."></input>
+                        </div>
+
+                        <div className="col-auto px-0 mx-0 mr-2">
+                            <button type="button" onClick={clickMe} className="btn btn-primary">Add Task</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="p-2 mx-4 border-black-25 border-bottom"></div>
+
+            <div className="TaskList container">
+                
+
+                <div class="table-wrapper">
+                    <table className="table table-hover table-bordered">
+                        <thead>
+                            <tr>
+                                <th>No.</th>
+                                <th>Todo item</th>
+                                
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        {lists &&
+                        <tbody>
+                             {lists.map((list) => (
+                            <tr>
+                                <td>{list.id}</td>
+                               
+                                <td className="" key={list.id}>
+                                {list.title}
+                                </td>
+                                
+                                
+                                <td>
+                                    <button className="btn btn-danger" >
+                                        Delete
+                                    </button>
+                                    <button className="btn btn-success" >
+                                        Finished
+                                    </button>
+                                </td>
+                            </tr>
+                            ))}
+                        
+                        </tbody>
+                        }
+                    </table>
+                </div>
+            </div>
+
         </div>
     );
 }
 
-export default Home; 
+export default Home;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,22 @@
-const Navbar = () =>{
-    return (
-        <nav className="navbar">
-            <h1>ToDo List</h1>
-            <div className="links">
-                <a href="/">Home</a>
-                <a href="/task">Task</a>
-            </div>
-        </nav>
-    );
+const Navbar = () => {
+  return (
+
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <a className="navbar-brand" href="#">TODO List</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <a className="nav-item nav-link active" href="#">Home</a>
+          <a className="nav-item nav-link" href="#">Features</a>
+          <a className="nav-item nav-link" href="#">Pricing</a>
+          <a className="nav-item nav-link disabled" href="#">Disabled</a>
+        </div>
+      </div>
+    </nav>
+
+  );
 }
 
-export default Navbar; 
+export default Navbar;
